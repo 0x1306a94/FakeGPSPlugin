93109f4ebf024888b8b670bfd866014039c4dae3
0x1306a94 <0x1306a94@gmail.com>
2019-08-02 15:42:31 +0800

    初始化工程

---
+++ FakeGPS/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  FakeGPS
+//
+//  Created by king on 2019/8/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
